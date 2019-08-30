0d0d98793bdae98ba8559a0b9a9b56c3dece60e8
zhouyiqi91 <root@SGR09.sgr.com>
2019-08-30 07:08:23 +0000

    modify remove_doublets

--- remove_doublets.R
+++ remove_doublets.R
@@ -8,6 +8,7 @@ argv <- add_argument(argv,"--rds",help="Seurat rds")
 argv <- add_argument(argv,"--detection_rate",help="detection_rate",default=0.5)
 argv <- add_argument(argv,"--save_rds",help="save no doublet rds",default="Y")
 argv <- add_argument(argv,"--maxcell_perident",help="detection_rate",default=5000)
+argv <- add_argument(argv,"--plot_umi",help="plot_umi",default="N")
 argv <- parse_args(argv)
 
 
@@ -17,30 +18,40 @@ rds <- readRDS(argv$rds)
 print ("read done.")
 detection_rate <- argv$detection_rate
 save_rds <- argv$save_rds
+maxcell_perident <- argv$maxcell_perident
+plot_umi <- argv$plot_umi
+print (plot_umi)
 
 meta <- rds@meta.data
 idents <- unique(meta$new_ident)
 
-top1500 <- head(rownames(rds@hvg.info),1500)
-print ("find markers")
-markers <- FindAllMarkers(rds,genes.use = top1500,max.cells.per.ident=argv$maxcell_perident,min.diff.pct=0.5,only.pos=T)
-print ("find markers done.")
-markers$pct.diff <- markers$pct.1 - markers$pct.2
+hvg <- rownames(rds@hvg.info)
+top1500 <- head(hvg,1500)
+#print ("find markers")
+#markers <- FindAllMarkers(rds,genes.use = top1500,max.cells.per.ident=maxcell_perident,min.diff.pct=0.5,only.pos=T)
+#print ("find markers done.")
+#markers$pct.diff <- markers$pct.1 - markers$pct.2
 
 doublets <- list()
 for (ident1 in idents)
 {
+  doublets[[ident1]] <- list()
   rest_ident <- idents[!idents %in% ident1]
   for (ident2 in rest_ident)
   {
     list_name <- paste(ident1,ident2,sep="_")
     print (list_name)
-    ident2_marker <- markers[markers$cluster==ident2,]
-    ident2_top_genes <- ident2_marker$gene
+    #ident2_marker <- markers[markers$cluster==ident2,]
+    print ("find_marker genes")
+    ident2_marker <- FindMarkers(rds,ident.1=ident2,ident.2=ident1,
+      genes.use = top1500,max.cells.per.ident=maxcell_perident,min.diff.pct=0.5,only.pos=T)
+    print ("find marker genes done.")
+    ident2_top_genes <- rownames(ident2_marker)
     print (ident2_top_genes)
     dropout_percent <- mean(ident2_marker$pct.1)
     find_number <- round(length(ident2_top_genes)*dropout_percent*detection_rate) 
-    print (find_number)
+    print (paste0("min find gene number: ",find_number))
+    print ("")
     if (is.nan(find_number)) {next}
     if (find_number==0) {find_number <- 1}
     ident1_barcode <- rownames(meta[meta$new_ident==ident1,])
@@ -70,7 +81,31 @@ for (ident1 in idents)
       }
     }
     doublet_barcode <- names(barcode_count[barcode_count>=find_number])
-    doublets[[list_name]] <- doublet_barcode
+    doublets[[ident1]][[ident2]] <- doublet_barcode
+    # plot_umi
+
+    if (plot_umi!="N"){
+      ident2_barcode <- rownames(meta[meta$new_ident==ident2,])
+      temp_data <- rds@raw.data[ident2_top_genes,c(ident1_barcode,ident2_barcode)]
+      all_tibble <- tibble(gene=character(),umi=numeric(),data=character())
+      for (gene in ident2_top_genes){
+        ident1_umi <- temp_data[gene,ident1_barcode]
+        ident2_umi <- temp_data[gene,ident2_barcode]
+        doublet_umi <- temp_data[gene,doublet_barcode]
+        data1 <- tibble(gene=gene,umi=ident1_umi,data=ident1)
+        data2 <- tibble(gene=gene,umi=ident2_umi,data=ident2)
+        data3 <- tibble(gene=gene,umi=doublet_umi,data="doublets")
+        temp_tibble <- rbind(data1,data2,data3)
+        all_tibble <- rbind(all_tibble,temp_tibble)
+      }
+      pdf_name <- paste0(list_name,".pdf")
+      print (pdf_name)
+      pdf(pdf_name,width=15)
+      print (ggplot(all_tibble,aes(x=gene,y=umi,fill=data)) + geom_boxplot() + coord_flip() )
+      dev.off()
+    }
+
+
   }
 }
 
@@ -81,21 +116,52 @@ for (item in doublets)
 }
 
 named_vector <- c()
-for (item in names(doublets))
+for (item1 in names(doublets))
 {
-  for (barcode in doublets[[item]])
+  for (item2 in names(doublets[[item1]]))
   {
-    
-    if ( barcode %in% names(named_vector) ){
-      value <- paste(named_vector[barcode],item,sep=" , ")
+    for (barcode in doublets[[item1]][[item2]])
+    {
+      if ( barcode %in% names(named_vector) )
+      {
+      value <- paste(named_vector[barcode],item2,sep=",")
       named_vector[barcode] <- value
-    } else{
-      named_vector[barcode] <- item
+      } else {
+      value <- paste(item1,item2,sep=",")
+      named_vector[barcode] <- value
+      }
     }
   }
 }
+
+doublets_vector <- names(named_vector)
+
+cell_count <- c()
+for (doublets in named_vector)
+{
+  attrs <- unlist(strsplit(doublets,","))
+  n <- length(attrs)
+  for (attr in attrs){
+    if (attr %in% names(cell_count)){
+      cell_count[attr] <- cell_count[attr] + (1/n)
+    } else {
+      cell_count[attr] <- 1/n
+    }
+  }
+}
+cell_dat <- tibble(cell=names(cell_count),count=cell_count)
+write_tsv(cell_dat,"doublet_cell_count.txt")
+
+
 named_dat <- data.frame(barcode=names(named_vector),identity=named_vector)
 write_tsv(named_dat,"doublets_identity.txt")
+
+
+
+
+
+
+
 
 saveRDS(doublets,"doublets.rds")
 
