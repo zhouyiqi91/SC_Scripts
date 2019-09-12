5faadd24b7af70d0f218c22bfc4581fc30b5949f
zhouyiqi91 <root@SGR09.sgr.com>
2019-09-12 13:44:54 +0800

    add lung_cancer_signature.R

---
+++ lung_cancer_signature.R
@@ -0,0 +1,16 @@
+luad <- readRDS("/SGRNJ01/Aftersales/P2018009_Lung/20190624_jiace/paper/rerun/luad/LUAD.rds")
+
+genes <- readRDS("/SGRNJ/Database/sc_marker/alveolar/alveolar_gene.rds")
+a_gene <- genes$normal
+l_gene <- genes$cancer
+TSNEPlot(luad,do.label = T)
+
+y <- luad@data[gene,]
+a_mean <- colMeans(as.matrix(y[a_gene,]), na.rm = FALSE, dims = 1)
+l_mean <- colMeans(as.matrix(y[l_gene,]), na.rm = FALSE, dims = 1)
+a_mean[a_mean==0] <- min(a_mean[a_mean!=0])
+l_mean[l_mean==0] <- min(l_mean[l_mean!=0])
+res <- (log(a_mean,base = 2) - log(l_mean,base=2))
+luad@meta.data$signature <- res
+FeaturePlot(luad,"signature",no.legend = F,cols.use = c("red","green"))
+TSNEPlot(luad,group.by="samples",no.legend = F,colors.use = color2,do.label=T)
